aab24896249efc490b2b1156ed7b6c958db0075e
L-yushing-chen <Elsa.Chen@Libertyits.com>
2020-09-02 21:40:52 -0400

    Update smart-app.js

--- src/js/smart-app.js
+++ src/js/smart-app.js
@@ -7,79 +7,105 @@
       ret.reject();
     }
 
-    function onReady(smart)  {
-      alert(smart.userId);
-      alert(smart.tokenResponse.access_token);
+    function onReady(smart)  {  
+      alert(JSON.stringify(smart));
+
+      if (smart.hasOwnProperty('patient')) {
+        console.log(JSON.stringify(smart));
+        var patient = smart.patient;
+        var pt = patient.read();
+
+        $.when(pt).fail(onError);
+        
+        $.when(pt).done(function(patient) {
+            if (smart.hasOwnProperty('userId')) {
+              alert(smart.userId);
+              alert(smart.tokenResponse.access_token);
+              var settings = {
+                  "async": true,
+                  "url": smart.userId,
+                  //"url": "https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/12724069",
+                  "method": "GET",
+                  "headers": {
+                      "Content-Type": "application/json",
+                      "Accept": "application/json",
+                      "Authorization": "Bearer " + smart.tokenResponse.access_token
+                  },
+              }
+
+              $.ajax(settings).done(function (response) {
+                console.log("prationer ajax call ");
+                console.log(response);
+                alert(JSON.stringify(response));
+                if (typeof response.name[0] !== 'undefined') {
+                  var lName = response.name[0].family;
+                  patient.l5 = lName.substring(0, 5);
+                }
+                if (typeof response.identifier[0] !== 'undefined') {
+                  alert(response.identifier[0].value);
+                  var sn = response.identifier[0].value;
+                }
+  
+                patient.dz = response.id;
+                patient.sn = sn;
+                alert(JSON.stringify(patient));
+                alert(JSON.stringify(patient.resourceType));
+                ret.resolve(patient);
+              })
+            } else {
+              onError();
+            }
+        });
+      } else {
+        onError();
+      }
+    }
+    alert("version Rose-10-ES0902-1");
+    FHIR.oauth2.ready(onReady, onError);
+    return ret.promise();
+  };
+
+  function getPractitioner(patient) {
+      console.log(patient.userId);
+      console.log(patient.tokenResponse.id_token);
       var settings = {
           "async": true,
-          "url": smart.userId,
+          "url": patient.useId,
           "method": "GET",
           "headers": {
               "Content-Type": "application/json",
               "Accept": "application/json",
-              "Authorization": "Bearer " + smart.tokenResponse.access_token
+              "Authorization": "Bearer " + patient.tokenResponse.id_token
           },
       }
 
       $.ajax(settings).done(function (response) {
-          alert("practitioner ajax call ");
-          console.log(response);
-          alert(JSON.stringify(response));
-          var dz = response.id;
-          if (typeof response.name[0] !== 'undefined') {
-            var lName = response.name[0].family;
-            var l5 = lName.substring(0, 5);
-          }
+          console.log(response)
       })
-      .fail(function(data) {
-        if ( data.responseCode )
-        console.log( data.responseCode );
-      });
-      
-      if (smart.hasOwnProperty('patient')) {
-        alert(JSON.stringify(smart));
-        alert(JSON.stringify(smart.user));
-        console.log(JSON.stringify(smart));
-        var patient = smart.patient;
-        var pt = patient.read();
-        var user = smart.user;
-        var us = user.read();
-        $.when(pt).fail(onError);
-
-        $.when(pt).done(function(patient) {
-          ret.resolve(patient);
-          alert(JSON.stringify(patient));
-          alert(JSON.stringify(patient.resourceType));
-        });
-        //         $.when(us).fail(onError);
-//         $.when(us).done(function(user) {
-//           ret.resolve(user);
-//         });
-      } else {
-        alert("No patient exists");
-        onError();
-      }
-    }
-    alert('version 3');
-    FHIR.oauth2.ready(onReady, onError);
-    return ret.promise();
-
-  };
-
-  function getPractitioner(patient) {
-      if (typeof patient.careProvider[0] !== 'undefined') {
-        var pReference = patient.careProvider[0].reference;
-      }
-      var dz = pReference.split("/");
-      console.log(dz[1]);
-      return dz[1];
   }
+//function getPatientEDIPI(patient)
+ function getPatientEDIPI(patient) {
+      const dsvDodIdentifierSystemName = 'urn:oid:2.16.840.1.113883.3.42.10001.100001.12';
+      let patientId = 'undefined';
+      let found = false;
 
-  function getPatientICN(patient) {
-      const dsvIdentifierSystemName = 'urn:oid:2.16.840.1.113883.3.787.0.0';
-      const dsvIcnIdentifierSystemName = 'urn:oid:2.16.840.1.113883.4.349';
+      console.log('extracting the patient identifier EPIPI');
+      for(let i = 0; i < patient.identifier.length; i++) {
+        if (patient.identifier[i].system === dsvDodIdentifierSystemName) {
+          patientId = patient.identifier[i].value;
+          found = true;
+          console.log(patientId);
+          break;
+        }
+      }
+      return patientId;
+    }
+ 
+//function getPatientICN(patient)
+ function getPatientICN(patient) {
+     const dsvIcnIdentifierSystemName = 'urn:oid:2.16.840.1.113883.4.349'
 
-      let patientId = 'getting';
+      let patientId = 'undefined';
       let found = false;
 
       console.log('extracting the patient identifier ICN');
@@ -91,84 +117,77 @@
           break;
         }
       }
+      return patientId;
+    }
+ 
+//function getPatientMRN(patient) 
+ function getPatientMRN(patient) {
+  const dsvIdentifierSystemName = 'urn:oid:2.16.840.1.113883.3.787.0.0';
+  
+      let patientId = 'undefined';
+      let found = false;
 
-      if(!found) {
-        console.log('ICN not found.  extracting patient identifier MRN');
-        for(let i = 0; i < patient.identifier.length; i++) {
-          if (patient.identifier[i].system === dsvIdentifierSystemName) {
-            patientId = patient.identifier[i].value;
-            found = true;
-            console.log(patientId);
-            break;
-          }
+      console.log('extracting the patient identifier MRN');
+      for(let i = 0; i < patient.identifier.length; i++) {
+        if (patient.identifier[i].system === dsvIcnIdentifierSystemName) {
+          patientId = patient.identifier[i].value;
+          found = true;
+          console.log(patientId);
+          break;
         }
       }
-
-      if(!found) {
-        console.log('Not patient identifier found');
-      }
-
       return patientId;
+    }
 
-  }
-
-//window.redirectToRoes = function(patient) {
+//window.redirectToES = function(patient)
   window.redirectToES = function(patient) {
+      //alert(JSON.stringify(patient));
+      //console.log(JSON.stringify(patient));
+      //getPractitioner(patient);
       alert(JSON.stringify(patient));
-      console.log(JSON.stringify(patient));
+      var edipi = getPatientEDIPI(patient);
       var icn = getPatientICN(patient);
-      alert(icn);
+      var mrn = getPatientMRN(patient);
+      var deb_id = 'EDIPI=***' + edipi + '***ICN=***' + icn +  '***MRN***='  + mrn + '***' ;
+      alert(deb_id);
+	if ( (edipi == 'undefined' ) && ( icn == 'undefined' ) && ( mrn == 'undefined' )  )
+	{   console.log('Not patient identifier found');
+		alert('Redirect to Service Connected Rated Conditions of Enrollment will be faild; Due to Patient identifier Required.');
+      }
+	     
       var fname = '';
       var lname = '';
-      if (typeof patient.name[0] !== 'undefined') {
+ 
+      if (typeof patient.name[0] !== 'undefined' ) {
         fname = patient.name[0].given;
         lname = patient.name[0].family;
       }
-      var nm = lname + "," + fname;
+      var nm = fname + "+" + lname;
       console.log(nm);
 
-      var dobs = patient.birthDate.split("-");
-      var dob = dobs[0]-1700 + dobs[1] + dobs[2];
-      console.log(dob);
+      var gender = patient.gender;
 
-      var l1 = patient.address[0].line;
-      var ci = patient.address[0].city;
-      var st = "1^" + patient.address[0].state;
-      var zp = patient.address[0].postalCode;
-      
-      console.log(l5);
-      var sn = "668";
-      var dz = "6729895";
-      var l5 = "NALAM";
-    //Ec-add-ES-begin
-    //Data Url      
-    //edipi=12312313&icn=1008834677V755078&name=John+Adams&age=38&dob=10%2F10%2F1961&sex=M&fin=12345544&mrn=33355632221     
-    //Hostname Url
-    //https://usvadceapp.lcahncrls.net/?data=
-    //https://vaausappesr801.aac.va.gov:7401/es-rs/ratingapp/postform
-      var edipi="";
-      var sex="";
-      var fin="";
-      var mrn="";
-          
-          var es_dataUrl = "EDIPI=" + edipi + "ICN=" + icn + "&" + "NM=" + nm + "&" + "DOB=" + dob + "&" +"SEX=" + sex + "&" + "FIN=" + fin + "&" + "MRN=" + mrn  ;
-          var es_url = "https://usvadceapp.lcahncrls.net/?data="+dataUrl;
+      var dob = new Date(patient.birthDate);
+      var day = dob.getDate();
+      var monthIndex = dob.getMonth() + 1;
+      var year = dob.getFullYear();
+      var dobStr = monthIndex + '/' + day + '/' + year
+      console.log(dobStr);
+      var fin="";	
+      if (typeof patient.l5 !== 'undefined') 
+         var l5 = patient.l5.toUpperCase();
+//Data Url      
+//edipi=12312313&icn=1008834677V755078&name=John+Adams&age=38&dob=10%2F10%2F1961&sex=M&fin=12345544&mrn=33355632221     
+//Hostname Url
+//https://usvadceapp.lcahncrls.net/?data=
+//https://vaausappesr801.aac.va.gov:7401/es-rs/ratingapp/postform         
+var es_dataUrl = "edipi=" + edipi + "icn=" + icn + "&" + "name=" + nm + "&" + "dob=" + dobStr + "&" +"sex=" + gender + "&" + "fin=" + fin + "&" + "mrn=" + mrn  ;
+var es_url = "https://usvadceapp.lcahncrls.net/?data="+ es_dataUrl;
 
-              console.log(es_url);
-              alert(es_url);
-              window.location.replace(es_url);
-    //Ec-add-ES-end        
-          
-    //Ec-commentOut- begin
-//          var roes_url = "https://vaww.dalctest.oamm.va.gov/scripts/mgwms32.dll?MGWLPN=ddcweb&wlapp=roes3patient&" 
-//         + "ICN=" + icn + "&" + "NM=" + nm + "&" + "DOB=" + dob + "&" + "L1=" + l1 + "&" + "CI=" + ci + "&" + "ST=" + st + "&"
-//          + "ZP=" + zp + "&" + "DZ=" + dz + "&" + "L5=" + l5 + "&" + "SN=" + sn;
-
-//         console.log(roes_url);
-//          alert(roes_url);
-//          window.location.replace(roes_url);
-//Ec-commentOut- End
-  
+      console.log(es_url);
+      alert(es_url);
+//      window.location.replace(es_url);
   };
 
 })(window);
+
